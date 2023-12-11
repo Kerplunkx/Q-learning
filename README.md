053ba431f028c0d47d00d8291f58c2af27f9c14f
Guillermo Moreira <guillermomoreira01@gmail.com>
2023-12-11 12:32:17 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+Link colab: https://colab.research.google.com/drive/17ORcEjCZTvq5oEpe1LyZrLrOUrlWNPzV?usp=sharing
+.
---
+++ api_connector.py
@@ -0,0 +1,23 @@
+import requests
+from dotenv import load_dotenv
+from os import getenv
+
+load_dotenv()
+
+api_url = getenv("API_URL")
+
+def obtener_temperatura_actual():
+    resp = requests.get(api_url+'temp').json()
+    return int(resp['Message'])
+
+def obtener_estado_actual():
+    resp = requests.get(api_url+'estado').json()
+    return int(resp['Message'])
+
+def cambiar_temperatura_actual(nueva_temperatura):
+    if nueva_temperatura >= 16 and nueva_temperatura <=24:
+        requests.patch(url = api_url+'temp/'+str(nueva_temperatura))
+
+def cambiar_estado_actual(nuevo_estado):
+    if nuevo_estado == 0 or nuevo_estado == 1:
+        requests.patch(url = api_url+'estado/'+str(nuevo_estado))
